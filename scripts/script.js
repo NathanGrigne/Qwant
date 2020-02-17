b3617fe2207b36d6ae810a699b740ff05aad4dc1
nathangrigne <grigne.nathan@gmail.com>
2020-02-17 23:20:38 +0100

    upsate script

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const navigationLogo = document.querySelector('.logo-container-js')
+const navigationSeo = document.querySelector('.navigation-seo-js')
+const navigationRank = document.querySelector('.navigation-rank-js')
+const navigationQwant = document.querySelector('.navigation-qwant-js')
+const navigationContenders = document.querySelector('.navigation-contenders-js')
+
+navigationLogo.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+navigationSeo.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 491,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+navigationRank.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 1212,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+navigationQwant.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 2339,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+navigationContenders.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 3801,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
